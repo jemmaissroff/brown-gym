95486b25d7f7965a3d00b15215c2b1e30a07b960
Jemma Issroff <jemmaissroff@gmail.com>
2014-04-17 13:48:01 -0400

    Now stores and can upload different images

--- main.py
+++ main.py
@@ -16,8 +16,9 @@
 #
 import webapp2
 import models
-from google.appengine.ext.webapp import template
-from google.appengine.api import images
+import urllib
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
 # from google.appengine.ext import db
 
 class MainHandler(webapp2.RequestHandler):
@@ -41,22 +42,46 @@ class MainHandler(webapp2.RequestHandler):
                 <body>
                     There is %s someone using the treadmill
                     <p style="text-align:center">
-                        <img title="logo"  src="in_use.jpg" />
-                            hi ther 
+                        <img title="logo"  src=%r />
                         </img>
                     </p> 
                 </body>
             </html>"""
             for r in run_db.run(limit=1):
                 if(r.running == "True"):
-                    self.response.write(html_txt % "")
+                    self.response.write(html_txt % ("",
+                        "/serve/AMIfv96q4lsf7RIZPVtoH6P-dDqzLG_7yF0agkecoKKc4gwZYctt6EQ199ffN0HBJ5aIe6CHIsV9uoKXD_MOyo9OeZF3Krqd4jcT_p7xevqxooKIJPxxZJ53PN7Ppmd-9PJzoMb_WXPWd5jlWFxodXMP46UVMa-tmA"))
                 elif(r.running == "False"):
-                    self.response.write(html_txt % "not")
+                    self.response.write(html_txt % ("not",
+                        "serve/AMIfv94lTNQ7v8_IjtZ897dL-xAIu1tACMFZtGL3A2zWjO2xsRn-HsB8UnvIB-aOEbpqJH6k7S1nLaLRBuh1YJutURcxziJM9-awgjGaWpQSFIsH_zQN6xwJ_6mlSbQa0wcVCBhgk94eKpe2OmkxmuqnBJvw78EOMQ"))
                 else:
                     self.response.write("uh oh. not true or false")
 
+class UploadForm(webapp2.RequestHandler):
+  def get(self):
+    upload_url = blobstore.create_upload_url('/ready-for-upload')
+    self.response.out.write('<html><body>')
+    self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+    self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit"
+        name="submit" value="Submit"> </form></body></html>""")
+
+
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+  def post(self):
+    upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
+    blob_info = upload_files[0]
+    self.redirect('/serve/%s' % blob_info.key())
+
+
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+  def get(self, resource):
+    resource = str(urllib.unquote(resource))
+    blob_info = blobstore.BlobInfo.get(resource)
+    self.send_blob(blob_info)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/img', Image)
+    ('/upload', UploadForm),
+    ('/ready-for-upload', UploadHandler),
+    ('/serve/([^/]+)?', ServeHandler)
 ], debug=True)
